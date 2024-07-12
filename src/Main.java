8d7b691e5a2d887c685e999d4de0cfb954710800
Emir Karamoldo <emirkaramoldo@proton.me>
2024-07-12 13:53:26 +0600

    Выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println(permission(generateRandomAge(), -10));
+        System.out.println(permission(generateRandomAge(), -5));
+        System.out.println(permission(generateRandomAge(), 10));
+        System.out.println(permission(generateRandomAge(), 20));
+        System.out.println(permission(generateRandomAge(), 30));
+    }
+    public static int generateRandomAge() {
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+    public static String permission(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
         }
     }
 }
